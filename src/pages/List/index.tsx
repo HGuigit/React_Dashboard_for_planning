84bd2f482ea3a4ce6c792b664feba55cdfd8e0e9
Hguigit <guilhermesps2@gmail.com>
2021-05-12 05:32:28 -0300

    Initial commit

---
+++ src/pages/List/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const List: React.FC = () => {
+    return(
+        <div>
+        <h1>Dashboard</h1>
+
+        <h1>Dashboard</h1>
+        </div>
+    );
+
+}
+
+export default List;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const SignIn: React.FC = () => {
+
+    return (
+
+        <h1>List</h1>
+    
+    )
+
+}
+
+
+export default SignIn

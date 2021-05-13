86694677981bf3fdd5ef75846e2bd83dc98e80e9
Hguigit <guilhermesps2@gmail.com>
2021-05-13 03:57:47 -0300

    dev: added grid styling and started header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
-
 import Dashboard from './components/Layout';
+
+import dark from './styles/Themes/dark';
+import light from './styles/Themes/light';
 
 const App: React.FC = () => {
     
     return(
-        <>
+        <ThemeProvider theme={dark}>
             <GlobalStyles />
             <Dashboard />
-        </>
+        </ThemeProvider>
     )
 
 }   
--- src/components/MainHeader/styles.tsx
+++ src/components/MainHeader/styles.tsx
@@ -2,7 +2,42 @@ import styled from  'styled-components';
 
 export const Container = styled.div`
     grid-area:MH;
-    background-color: #252A48;
+    color: ${props => props.theme.colors.white};
+    background-color: ${props => props.theme.colors.secondary};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 10px;
+    border-bottom: 1px solid ${props=>props.theme.colors.gray};
 
 `;
+
+export const Profile = styled.div`
+    display: flex;
+    flex-direction: column;
+    color: ${props => props.theme.colors.white};
+
+
+
+
+`;
+
+export const Welcome = styled.div`
+    font-size: 20px;
+    font-weight: bold;
+
+
+
+
+`;
+
+export const UserName = styled.span`
+
+
+
+
+`;
+
+
+
 
